6a25d5a553ac648354a2b1b91698cee262fe63b9
hoffmannm <mjhoffmann@gmail.com>
2017-10-03 07:35:28 -0700

    Added mockup-content for detailed structure view

--- app/components/StructureView/index.js
+++ app/components/StructureView/index.js
@@ -5,16 +5,33 @@
 */
 
 import React from 'react';
-// import styled from 'styled-components';
 
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+/* import { FormattedMessage } from 'react-intl'; */
+/* import messages from './messages'; */
+
+import { Tabs, Tab } from 'material-ui/Tabs';
+
 
 function StructureView() {
   return (
     <div>
       <h2>Detailed Structure View</h2>
-      <FormattedMessage {...messages.header} />
+      <Tabs
+        tabItemContainerStyle={{ width: '900px' }}
+      >
+        <Tab label="Structure One">
+          <h2>... More on Structure 1</h2>
+        </Tab>
+        <Tab label="Structure Two">
+          <h2>... More on Structure 2</h2>
+        </Tab>
+        <Tab label="Structure Three">
+          <h2>... More on Structure 3</h2>
+        </Tab>
+        <Tab label="Structure Four">
+          <h2>... More on Structure 4</h2>
+        </Tab>
+      </Tabs>
     </div>
   );
 }
