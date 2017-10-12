b124cd6f8ccd45bda94325ea8472a54310573cfd
hoffmannm <mjhoffmann@gmail.com>
2017-10-11 23:37:21 -0700

    Started component for general search

--- app/components/GeneralSearch/index.js
+++ app/components/GeneralSearch/index.js
@@ -5,16 +5,65 @@
 */
 
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import { MdClear, MdSearch } from 'react-icons/lib/md';
+
+const Button = styled(RaisedButton)`
+  margin: 12px;
+`;
 
 class GeneralSearch extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div>
-        <FormattedMessage {...messages.header} />
+        <h2>General Search</h2>
+        <TextField hintText="Free Text Search" />
+        {'\t '}
+        <TextField hintText="Composition" />
+        <br />
+        <TextField hintText="Authors" />
+        {'\t '}
+        <TextField hintText="Title of Article" />
+        {'\t '}
+        <TextField hintText="Title of Journal" />
+        <br />
+
+        <SelectField
+          floatingLabelText="Facet"
+          floatingLabelStyle={{
+            fontFamily: 'Arial',
+          }}
+        >
+          <MenuItem value="any" primaryText="any" />
+          <MenuItem value="111" primaryText="(111)" />
+          <MenuItem value="100" primaryText="(100)" />
+          <MenuItem value="110" primaryText="(110)" />
+          <MenuItem value="210" primaryText="(210)" />
+          <MenuItem value="310" primaryText="(310)" />
+          <MenuItem value="other" primaryText="other" />
+        </SelectField>
+        <SelectField
+          floatingLabelText="Site"
+          floatingLabelStyle={{
+            fontFamily: 'Arial',
+          }}
+        >
+          <MenuItem value="any" primaryText="any" />
+          <MenuItem value="fcc" primaryText="fcc" />
+          <MenuItem value="hcp" primaryText="hcp" />
+          <MenuItem value="bridge" primaryText="bridge" />
+          <MenuItem value="hollow" primaryText="hollow" />
+          <MenuItem value="top" primaryText="top" />
+          <MenuItem value="other" primaryText="other" />
+        </SelectField>
+        <br />
+        <Button label="Clear" icon={<MdClear />} />
+        <Button label="Search" icon={<MdSearch />} />
       </div>
     );
   }
