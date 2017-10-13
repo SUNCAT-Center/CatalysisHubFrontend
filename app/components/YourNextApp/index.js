48c3be5be812192ded75cdb209bb8eba55d54eaf
hoffmannm <mjhoffmann@gmail.com>
2017-10-12 18:27:08 -0700

    Added placeholder app for YourNextApp

---
+++ app/components/YourNextApp/index.js
@@ -0,0 +1,28 @@
+/**
+*
+* YourNextApp
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+class YourNextApp extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <h2>Your Next App</h2>
+        <div>
+          {"Start mocking up the interface for your next SUNCAT app in 'app/components/YourNextApp/index.js'. Let's hang out on the catapp SLACK channel if you have any questions!"}
+        </div>
+      </div>
+    );
+  }
+}
+
+YourNextApp.propTypes = {
+
+};
+
+export default YourNextApp;
