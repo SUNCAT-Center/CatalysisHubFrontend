2a2d57b89a372a0211056439aa8a2dc350d58b07
hoffmannm <mjhoffmann@gmail.com>
2017-10-12 18:17:12 -0700

    Added more details for general energies page

--- app/containers/EnergiesPage/index.js
+++ app/containers/EnergiesPage/index.js
@@ -5,17 +5,80 @@
  */
 
 import React, { PropTypes } from 'react';
+import styled from 'styled-components';
+
 import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import { FaArrowsH } from 'react-icons/lib/fa';
+import RaisedButton from 'material-ui/RaisedButton';
+import { MdClear, MdSearch } from 'react-icons/lib/md';
+
 import makeSelectEnergiesPage from './selectors';
-import messages from './messages';
+const Button = styled(RaisedButton)`
+  margin: 12px;
+`;
 
 export class EnergiesPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div>
-        <FormattedMessage {...messages.header} />
+        <h2>Reaction Energies</h2>
+
+        <SelectField
+          floatingLabelText="Reactant"
+          floatingLabelStyle={{
+            fontFamily: 'Arial',
+          }}
+        >
+          <MenuItem value="CO*" primaryText="CO*" />
+          <MenuItem value="O*" primaryText="O*" />
+          <MenuItem value="H*" primaryText="H*" />
+          <MenuItem value="N*" primaryText="N*" />
+        </SelectField>
+        {' '}
+        <FaArrowsH
+          size={30}
+          style={{
+            margin: 15,
+            marginTop: -25,
+          }}
+        />
+        {' '}
+        <SelectField
+          floatingLabelText="Product A"
+          floatingLabelStyle={{
+            fontFamily: 'Arial',
+          }}
+        >
+          <MenuItem value="CO*" primaryText="CO*" />
+          <MenuItem value="O*" primaryText="O*" />
+          <MenuItem value="H*" primaryText="H*" />
+          <MenuItem value="N*" primaryText="N*" />
+        </SelectField>
+        {' '}
+        <SelectField
+          floatingLabelText="Product B"
+          floatingLabelStyle={{
+            fontFamily: 'Arial',
+          }}
+        >
+          <MenuItem value="CO*" primaryText="CO*" />
+          <MenuItem value="O*" primaryText="O*" />
+          <MenuItem value="H*" primaryText="H*" />
+          <MenuItem value="N*" primaryText="N*" />
+        </SelectField>
+        <br />
+        <SelectField
+          floatingLabelText="Surface"
+          floatingLabelStyle={{
+            fontFamily: 'Arial',
+          }}
+        >
+        </SelectField>
+        <Button label="Reset" icon={<MdClear />} />
+        <Button label="Search" icon={<MdSearch />} />
       </div>
     );
   }
