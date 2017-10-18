b4407e0f9788ea644b7b383f10cc9c8f8abf886b
Paul C. Jennings <jennings.p.c@gmail.com>
2017-10-17 19:37:29 -0700

    start atoml components

---
+++ app/components/AtoML/messages.js
@@ -0,0 +1,13 @@
+/*
+ * AtoML Messages
+ *
+ * This contains all the text for the AtoML component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.AtoML.header',
+    defaultMessage: 'This is the AtoML component !',
+  },
+});
