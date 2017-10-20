3c04b08e250fb9344cc47db9f1b2950f11c51c1f
hoffmannm <mjhoffmann@gmail.com>
2017-10-20 10:02:57 -0700

    Added forgotten ResultTable

---
+++ app/components/ResultTable/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import ResultTable from '../index';
+
+describe('<ResultTable />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
