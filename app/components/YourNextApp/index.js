c7ba226aab2ee589632c9c5be196bbb4e72a0e1b
Paul C. Jennings <jennings.p.c@gmail.com>
2017-10-20 10:42:06 -0700

    update stuff to material-ui v1.0

--- app/components/YourNextApp/index.js
+++ app/components/YourNextApp/index.js
@@ -5,38 +5,53 @@
 */
 
 import React from 'react';
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
+import styled from 'styled-components';
+
+import { MenuItem } from 'material-ui/Menu';
+import { InputLabel } from 'material-ui/Input';
+import Select from 'material-ui/Select';
+
+import { FormControl } from 'material-ui/Form';
+import Button from 'material-ui/Button';
 // import styled from 'styled-components';
 
 const supportAtoms = [
-  'Ag', 'Al', 'As', 'Au', 'B', 'Ba', 'Be', 'Bi', 'Ca', 'Cd',
-  'Co', 'Cr', 'Cs', 'Cu', 'Fe', 'Ga', 'Ge', 'Hf', 'Hg', 'In', 'Ir', 'K', 'La',
-  'Li', 'Mg', 'Mn', 'Mo', 'Na', 'Nb', 'Ni', 'O', 'Os', 'Pb', 'Pd', 'Pt', 'Rb',
-  'Re', 'Rh', 'Ru', 'Sb', 'Sc', 'Si', 'Sn', 'Sr', 'Ta', 'Te', 'Ti', 'Tl', 'V',
-  'W', 'Y', 'Zn', 'Zr',
+  'Ag', 'Al', 'As', 'Au', 'B', 'Ba', 'Be', 'Bi', 'Ca', 'Cd', 'Co', 'Cr', 'Cs',
+  'Cu', 'Fe', 'Ga', 'Ge', 'Hf', 'Hg', 'In', 'Ir', 'K', 'La', 'Li', 'Mg', 'Mn',
+  'Mo', 'Na', 'Nb', 'Ni', 'O', 'Os', 'Pb', 'Pd', 'Pt', 'Rb', 'Re', 'Rh', 'Ru',
+  'Sb', 'Sc', 'Si', 'Sn', 'Sr', 'Ta', 'Te', 'Ti', 'Tl', 'V', 'W', 'Y', 'Zn',
+  'Zr',
 ];
 
 const supportLength = supportAtoms.length;
 
-// const styles = {
-//  customWidth: {
-//    width: 150,
-//  },
-// };
-
 const items = [];
 for (let i = 0; i < supportLength; i += 1) {
-  items.push(<MenuItem value={supportAtoms[i]} primaryText={supportAtoms[i]} />);
+  items.push(<MenuItem value={supportAtoms[i]}>{supportAtoms[i]}</MenuItem>);
 }
 
 
 class YourNextApp extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  // state = {
-  //   value: 1,
-  // };
+  constructor(props) {
+    super(props);
+    this.state = {
+      m1: '',
+      m2: '',
+      facet: '',
+      ads: '',
+      conc: '',
+      site: '',
+      results: [],
+    };
+  }
 
-  handleChange = (event, index, value) => this.setState({ value });
+  handleChange(name) {
+    return (event) => {
+      this.setState({
+        [name]: event.target.value,
+      });
+    };
+  }
 
   render() {
     return (
@@ -44,91 +59,107 @@ class YourNextApp extends React.Component { // eslint-disable-line react/prefer-
         <h2>AtoML</h2>
         <div>
           {'Mockup of an AtoML app.'}
-        </div>
+          <br />
 
-        <div>
-          <SelectField
-            floatingLabelText="First Support"
-            // value={this.state.value}
-            // onChange={this.handleChange}
-            maxHeight={200}
+          <FormControl
+            style={{ minWidth: 120, margin: 12 }}
           >
-            {items}
-          </SelectField>
+            <InputLabel>Metal One</InputLabel>
+            <Select
+              onChange={this.handleChange('m1')}
+              value={this.state.m1}
+            >
+              {items}
+            </Select>
+          </FormControl>
+          {'   '}
+          <FormControl
+            style={{ minWidth: 120 }}
+          >
+            <InputLabel>Metal Two</InputLabel>
+            <Select
+              onChange={this.handleChange('m2')}
+              value={this.state.m2}
+            >
+              {items}
+            </Select>
+          </FormControl>
+          {'   '}
+          <FormControl
+            style={{ minWidth: 120 }}
+          >
+            <InputLabel>Facet</InputLabel>
+            <Select
+              onChange={this.handleChange('facet')}
+              value={this.state.facet}
+            >
+              <MenuItem value="0001">0001</MenuItem>
+              <MenuItem value="0001step">0001step</MenuItem>
+              <MenuItem value="100">100</MenuItem>
+              <MenuItem value="110">110</MenuItem>
+              <MenuItem value="111">111</MenuItem>
+              <MenuItem value="211">211</MenuItem>
+              <MenuItem value="311">311</MenuItem>
+              <MenuItem value="532">532</MenuItem>
+            </Select>
+          </FormControl>
           <br />
-          <SelectField
-            floatingLabelText="Second Support"
-            // value={this.state.value}
-            // onChange={this.handleChange}
-            // disabled={false}
-            maxHeight={200}
+          <FormControl
+            style={{ minWidth: 120, margin: 12 }}
           >
-            {items}
-          </SelectField>
+            <InputLabel>Adsorbate</InputLabel>
+            <Select
+              onChange={this.handleChange('ads')}
+              value={this.state.ads}
+            >
+              <MenuItem value="graphene">graphene</MenuItem>
+              <MenuItem value="CH2CH2">CH2CH2</MenuItem>
+              <MenuItem value="CH3CH2CH3">CH3CH2CH3</MenuItem>
+              <MenuItem value="CH3CH3">CH3CH3</MenuItem>
+              <MenuItem value="CO">CO</MenuItem>
+              <MenuItem value="CO2">CO2</MenuItem>
+              <MenuItem value="H2O">H2O</MenuItem>
+              <MenuItem value="HCN">HCN</MenuItem>
+              <MenuItem value="NH3">NH3</MenuItem>
+              <MenuItem value="NO">NO</MenuItem>
+              <MenuItem value="O2">O2</MenuItem>
+              <MenuItem value="O">O</MenuItem>
+            </Select>
+          </FormControl>
+          {'   '}
+          <FormControl
+            style={{ minWidth: 120 }}
+          >
+            <InputLabel>Concentration</InputLabel>
+            <Select
+              onChange={this.handleChange('conc')}
+              value={this.state.conc}
+            >
+              <MenuItem value="0.25">0.25</MenuItem>
+              <MenuItem value="0.50">0.50</MenuItem>
+              <MenuItem value="0.75">0.75</MenuItem>
+            </Select>
+          </FormControl>
+          {'   '}
+          <FormControl
+            style={{ minWidth: 120 }}
+          >
+            <InputLabel>Site</InputLabel>
+            <Select
+              onChange={this.handleChange('site')}
+              value={this.state.site}
+            >
+              <MenuItem value="AA">AA</MenuItem>
+              <MenuItem value="BA">BA</MenuItem>
+              <MenuItem value="BB">BB</MenuItem>
+            </Select>
+          </FormControl>
           <br />
-          <SelectField
-            floatingLabelText="Facet"
-            // value={this.state.value}
-            // onChange={this.handleChange}
-            // style={styles.customWidth}
-          >
-            <MenuItem value={'0001'} primaryText="0001" />
-            <MenuItem value={'0001step'} primaryText="0001step" />
-            <MenuItem value={'100'} primaryText="100" />
-            <MenuItem value={'110'} primaryText="110" />
-            <MenuItem value={'111'} primaryText="111" />
-            <MenuItem value={'211'} primaryText="211" />
-            <MenuItem value={'311'} primaryText="311" />
-            <MenuItem value={'532'} primaryText="532" />
-          </SelectField>
           <br />
-          <SelectField
-            floatingLabelText="Adsorbate"
-            // value={this.state.value}
-            // onChange={this.handleChange}
-            // autoWidth={true}
-          >
-            <MenuItem value={'graphene'} primaryText="graphene" />
-            <MenuItem value={'CH2CH2'} primaryText="CH2CH2" />
-            <MenuItem value={'CH3CH2CH3'} primaryText="CH3CH2CH3" />
-            <MenuItem value={'CH3CH3'} primaryText="CH3CH3" />
-            <MenuItem value={'CO'} primaryText="CO" />
-            <MenuItem value={'CO2'} primaryText="CO2" />
-            <MenuItem value={'H2O'} primaryText="H2O" />
-            <MenuItem value={'HCN'} primaryText="HCN" />
-            <MenuItem value={'NH3'} primaryText="NH3" />
-            <MenuItem value={'NO'} primaryText="NO" />
-            <MenuItem value={'O2'} primaryText="O2" />
-            <MenuItem value={'O'} primaryText="O" />
-          </SelectField>
-          <br />
-          <SelectField
-            floatingLabelText="Concentration"
-            // value={this.state.value}
-            // onChange={this.handleChange}
-            // autoWidth={true}
-          >
-            <MenuItem value={'0.25'} primaryText="0.25" />
-            <MenuItem value={'0.50'} primaryText="0.50" />
-            <MenuItem value={'0.75'} primaryText="0.75" />
-          </SelectField>
-          <br />
-          <SelectField
-            floatingLabelText="Site"
-            // value={this.state.value}
-            // onChange={this.handleChange}
-            // autoWidth={true}
-          >
-            <MenuItem value={'AA'} primaryText="AA" />
-            <MenuItem value={'AB'} primaryText="AB" />
-            <MenuItem value={'BB'} primaryText="BB" />
-          </SelectField>
+          <Button raised style={{ margin: 12 }}>
+            Calculate
+          </Button>
         </div>
-
-        <div>
-          {'Below will give output from the GP model.'}
-        </div>
-
       </div>
     );
   }
