f86dadc99b272acc35b4edd1f2301f5214dd6684
hoffmannm <mjhoffmann@gmail.com>
2017-10-26 14:11:06 -0700

    Prettied up printing of publications list

--- app/components/Publications/index.js
+++ app/components/Publications/index.js
@@ -10,6 +10,20 @@ import React from 'react';
 import axios from 'axios';
 import { graphQLRoot } from 'utils/constants';
 
+const prettyPrintReference = (reference) => {
+  const ref = JSON.parse(reference);
+  // TODO Integrate with crossref.org api
+  // if (false && typeof ref.doi === 'undefined' || ref.doi === '') {
+  return (<span>
+    {ref.title !== '' ? <emph>{ref.title}. </emph> : null }
+    {typeof ref.authors !== 'undefined' ? <span>{ref.authors.join('; ')}. </span> : null }
+    {ref.journal !== '' ? <i>{ref.journal}, </i> : null }
+    {ref.volume !== '' ? <span>{ref.volume}, </span> : null}
+    {ref.pages !== '' ? <span>{ref.pages}, </span> : null}
+    {ref.year !== '' ? <span>{ref.year}.</span> : null}
+  </span>);
+};
+
 
 class Publications extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -46,14 +60,15 @@ class Publications extends React.Component { // eslint-disable-line react/prefer
   render() {
     return (
       <div>
-        {this.state.years.map((year) => (
-          <div>
-            <h2 key={`year_${year}`}>{year}</h2>
-            {(this.state.references[year] || []).map((reference) => (
-              <div>
-                <li>{reference}</li>
+        {this.state.years.map((year, i) => (
+          <div key={`div_year_${i}`}>
+            <h2 key={`pyear_${year}`}>{year}</h2>
+            {(this.state.references[year] || []).map((reference, j) => (
+              <div key={`pdiv_${i}_${j}`}>
+                <li key={`pli_${i}_${j}`}>{prettyPrintReference(reference)}</li>
               </div>
             ))}
+
           </div>
         ))
         }
