676d19d7ee9acc1b0148ec9d1831af71f493e1d7
hoffmannm <mjhoffmann@gmail.com>
2017-10-31 10:28:00 -0700

    Added custom App/Img component

---
+++ app/containers/App/Img.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import NormalImg from 'components/Img';
+
+const Img = styled(NormalImg)`
+  width: 240px;
+  margin: 0 auto;
+  display: block;
+`;
+
+export default Img;
