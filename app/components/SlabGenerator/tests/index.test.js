4c9bc2f30da8bc34b73d2030b1920a552422f8cf
Max Hoffmann <mjhoffmann@gmail.com>
2017-11-07 14:39:20 -0800

    Added stub of slab generator component

---
+++ app/components/SlabGenerator/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import SlabGenerator from '../index';
+
+describe('<SlabGenerator />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
