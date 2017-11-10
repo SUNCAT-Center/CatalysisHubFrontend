e40aaeac12696df9e7da965c42a60951483b69ec
Max Hoffmann <mjhoffmann@gmail.com>
2017-11-10 10:47:05 -0800

    Expanded generic app instructions

--- app/components/YourNextApp/index.js
+++ app/components/YourNextApp/index.js
@@ -14,7 +14,9 @@ class YourNextApp extends React.Component { // eslint-disable-line react/prefer-
       <div>
         <h2>Your Next App</h2>
         <div>
-          {"Start mocking up the interface for your next SUNCAT app in 'app/components/YourNextApp/index.js'. Let's hang out on the catapp SLACK channel if you have any questions!"}
+          {`Start mocking up the interface for your next SUNCAT app in 'app/components/YourNextApp/index.js'. Let's hang out on the catapp SLACK channel if you have any questions!
+          You will need a recent node version 8, and npm version 5 as a development environment.
+              `}
         </div>
       </div>
     );
