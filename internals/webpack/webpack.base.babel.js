dc534261ff887c80d1bc0e96c68d76c42a72d5b8
Max Hoffmann <mjhoffmann@gmail.com>
2017-11-14 21:09:12 -0800

    Installed autoSuggest component

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -95,4 +95,7 @@ module.exports = (options) => ({
   devtool: options.devtool,
   target: 'web', // Make web variables accessible to webpack, e.g. window
   performance: options.performance || {},
+  node: {
+    fs: 'empty',
+  },
 });
