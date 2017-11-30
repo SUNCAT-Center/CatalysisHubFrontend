6dac01807ad61c4aca8eba1b49a8e98b15db1d79
Max Hoffmann <mjhoffmann@gmail.com>
2017-11-30 13:04:35 -0800

    Return documents to make it easier to test

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,11 +1,14 @@
-function download(filename, text) {
+function download(filename, text, click = true) {
   const element = document.createElement('a');
   element.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`);
   element.setAttribute('download', filename);
   element.style.display = 'none';
   document.body.appendChild(element);
-  element.click();
+  if (click) {
+    element.click();
+  }
   document.body.removeChild(element);
+  return document
 }
 
 module.exports = { download };
