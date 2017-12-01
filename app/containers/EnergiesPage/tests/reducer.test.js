ec91ac7c66b977fdffa3d8f93bdb9026183c52bb
Max Hoffmann <mjhoffmann@gmail.com>
2017-11-30 16:32:21 -0800

    Added more unit tests

--- app/containers/EnergiesPage/tests/reducer.test.js
+++ app/containers/EnergiesPage/tests/reducer.test.js
@@ -1,6 +1,6 @@
 
 import energiesPageReducer from '../reducer';
-import * as actions from '../constants';
+import * as constants from '../constants';
 
 describe('energiesPageReducer', () => {
   it('returns the initial state', () => {
@@ -12,32 +12,32 @@ describe('energiesPageReducer', () => {
     }));
   });
   it('handles DEFAULT_ACTION ', () => {
-    const defaultAction = { type: actions.DEFAULT_ACTION };
+    const defaultAction = { type: constants.DEFAULT_ACTION };
     expect(energiesPageReducer({}, defaultAction)).toEqual({});
   });
   it('handles RECEIVE_SYSTEMS', () => {
-    const receiveSystems = { type: actions.RECEIVE_SYSTEMS };
+    const receiveSystems = { type: constants.RECEIVE_SYSTEMS };
     expect(energiesPageReducer({}, receiveSystems)).toEqual({});
   });
   it('handles RECEIVE_REACTIONS', () => {
-    const receiveReactions = { type: actions.RECEIVE_REACTIONS };
+    const receiveReactions = { type: constants.RECEIVE_REACTIONS };
     expect(energiesPageReducer({}, receiveReactions)).toEqual({});
   });
   it('handles CLEAR_SYSTEMS', () => {
-    const clearSystems = { type: actions.CLEAR_SYSTEMS };
+    const clearSystems = { type: constants.CLEAR_SYSTEMS };
     expect(energiesPageReducer({}, clearSystems)).toEqual({ reactionSystems: [] });
   });
   it('handles SELECT_REACTION', () => {
-    const selectReaction = { type: actions.SELECT_REACTION };
+    const selectReaction = { type: constants.SELECT_REACTION };
     expect(energiesPageReducer({}, selectReaction)).toEqual({});
   });
   it('handles SAVE_SYSTEM', () => {
-    const saveSystem = { type: actions.SAVE_SYSTEM, payload: 'System' };
+    const saveSystem = { type: constants.SAVE_SYSTEM, payload: 'System' };
     const rs = [{ mass: 1, volume: 1 }, { mass: 2, volume: 1 }, {}];
     expect(energiesPageReducer({ reactionSystems: rs }, saveSystem)).toEqual({ reactionSystems: [{}, 'System', { mass: 2, volume: 1 }, { mass: 1, volume: 1 }] });
   });
   it('handles SUBMIT_SEARCH', () => {
-    const submitSearch = { type: actions.SUBMIT_SEARCH };
+    const submitSearch = { type: constants.SUBMIT_SEARCH };
     expect(energiesPageReducer({}, submitSearch)).toEqual({ searchSubmitted: true });
   });
 });
--- app/containers/GeneralSearchContainer/tests/reducer.test.js
+++ app/containers/GeneralSearchContainer/tests/reducer.test.js
@@ -1,5 +1,8 @@
 
 import generalSearchContainerReducer from '../reducer';
+import * as constants from '../constants';
+
+const reducer = generalSearchContainerReducer;
 
 describe('generalSearchContainerReducer', () => {
   it('returns the initial state', () => {
@@ -8,5 +11,17 @@ describe('generalSearchContainerReducer', () => {
       searchResults: [],
       selectedSystem: {},
     }));
+  });
+  it('handles DEFAULT_ACTION', () => {
+    expect(reducer({}, { type: constants.DEFAULT_ACTION })).toEqual({});
+  });
+  it('handles SELECT_UUID', () => {
+    expect(reducer({}, { type: constants.SELECT_UUID })).toEqual({});
+  });
+  it('handles RECEIVE_RESULTS', () => {
+    expect(reducer({}, { type: constants.RECEIVE_RESULTS })).toEqual({});
+  });
+  it('handles SAVE_SYSTEMS', () => {
+    expect(reducer({}, { type: constants.SAVE_SYSTEMS })).toEqual({});
   });
 });
