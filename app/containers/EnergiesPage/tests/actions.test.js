2bb8d0cccb42a50f1dce447d2810f8099753294e
Max Hoffmann <mjhoffmann@gmail.com>
2017-11-30 16:07:17 -0800

    Added unittests on EnergiesPage redux components

--- app/containers/EnergiesPage/tests/actions.test.js
+++ app/containers/EnergiesPage/tests/actions.test.js
@@ -1,18 +1,52 @@
 
-import {
-  defaultAction,
-} from '../actions';
-import {
-  DEFAULT_ACTION,
-} from '../constants';
+import * as actions from '../actions';
+import * as constants from '../constants';
 
 describe('EnergiesPage actions', () => {
   describe('Default Action', () => {
     it('has a type of DEFAULT_ACTION', () => {
       const expected = {
-        type: DEFAULT_ACTION,
+        type: constants.DEFAULT_ACTION,
       };
-      expect(defaultAction()).toEqual(expected);
+      expect(actions.defaultAction()).toEqual(expected);
+    });
+  });
+  describe('Receive Systems', () => {
+    it('has a type of RECEIVE_SYSTEMS', () => {
+      const expected = {
+        type: constants.RECEIVE_SYSTEMS,
+      };
+      expect(actions.receiveSystems()).toEqual(expected);
+    });
+  });
+
+  describe(' submitSearch ', () => {
+    it('has type SUBMIT_SEARCH', () => {
+      expect(actions.submitSearch()).toEqual({ type: constants.SUBMIT_SEARCH });
+    });
+  });
+
+  describe('selectReaction', () => {
+    it('has type SELECT_REACTION', () => {
+      expect(actions.selectReaction()).toEqual({ type: constants.SELECT_REACTION });
+    });
+  });
+
+  describe('receiveReactions', () => {
+    it('has type RECEIVE_REACTIONS', () => {
+      expect(actions.receiveReactions()).toEqual({ type: constants.RECEIVE_REACTIONS });
+    });
+  });
+
+  describe('clearSystems', () => {
+    it('has type CLEAR_SYSTEMS', () => {
+      expect(actions.clearSystems()).toEqual({ type: constants.CLEAR_SYSTEMS });
+    });
+  });
+
+  describe('saveSystem', () => {
+    it('has type SAVE_SYSTEM', () => {
+      expect(actions.saveSystem()).toEqual({ type: constants.SAVE_SYSTEM });
     });
   });
 });
--- app/containers/EnergiesPage/tests/reducer.test.js
+++ app/containers/EnergiesPage/tests/reducer.test.js
@@ -1,5 +1,6 @@
 
 import energiesPageReducer from '../reducer';
+import * as actions from '../constants';
 
 describe('energiesPageReducer', () => {
   it('returns the initial state', () => {
@@ -10,4 +11,34 @@ describe('energiesPageReducer', () => {
       reactionSystems: [],
     }));
   });
+  it('handles DEFAULT_ACTION ', () => {
+    const defaultAction = { type: actions.DEFAULT_ACTION };
+    expect(energiesPageReducer({}, defaultAction)).toEqual({});
+  });
+  it('handles RECEIVE_SYSTEMS', () => {
+    const receiveSystems = { type: actions.RECEIVE_SYSTEMS };
+    expect(energiesPageReducer({}, receiveSystems)).toEqual({});
+  });
+  it('handles RECEIVE_REACTIONS', () => {
+    const receiveReactions = { type: actions.RECEIVE_REACTIONS };
+    expect(energiesPageReducer({}, receiveReactions)).toEqual({});
+  });
+  it('handles CLEAR_SYSTEMS', () => {
+    const clearSystems = { type: actions.CLEAR_SYSTEMS };
+    expect(energiesPageReducer({}, clearSystems)).toEqual({ reactionSystems: [] });
+  });
+  it('handles SELECT_REACTION', () => {
+    const selectReaction = { type: actions.SELECT_REACTION };
+    expect(energiesPageReducer({}, selectReaction)).toEqual({});
+  });
+  it('handles SAVE_SYSTEM', () => {
+    const saveSystem = { type: actions.SAVE_SYSTEM, payload: 'System' };
+    const rs = [{ mass: 1, volume: 1 }, { mass: 2, volume: 1 }, {}];
+    expect(energiesPageReducer({ reactionSystems: rs }, saveSystem)).toEqual({ reactionSystems: [{}, 'System', { mass: 2, volume: 1 }, { mass: 1, volume: 1 }] });
+  });
+  it('handles SUBMIT_SEARCH', () => {
+    const submitSearch = { type: actions.SUBMIT_SEARCH };
+    expect(energiesPageReducer({}, submitSearch)).toEqual({ searchSubmitted: true });
+  });
 });
+
