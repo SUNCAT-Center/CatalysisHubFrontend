fb44ae0585cf9d7d88ab55a961a48577cbe4c833
Max Hoffmann <mjhoffmann@gmail.com>
2017-12-05 23:00:50 -0800

    Fixed special characters on publication page

--- app/components/Publications/index.js
+++ app/components/Publications/index.js
@@ -33,18 +33,42 @@ const styles = (theme) => ({
   },
 });
 
+const restoreSC = (str) => {
+  let res = str;
+  if (str === null || typeof str === 'undefined') {
+    return '';
+  }
+  if (typeof str === 'object') {
+    res = str.join(' ');
+  }
+  return res
+  .replace('{\\o}', 'ø')
+  .replace('\\o', 'ø')
+  .replace('{"A}', 'Ä')
+  .replace('{"U}', 'Ü')
+  .replace('{"O}', 'Ö')
+  .replace('{"a}', 'ä')
+  .replace('{"u}', 'ü')
+  .replace('{"o}', 'ö')
+  .replace('{\\ss}', 'ß')
+  .replace('--', '–')
+
+  .replace('{', '')
+  .replace('}', '');
+};
+
 const prettyPrintReference = (reference) => {
   if (reference.indexOf('{') > -1) {
     const ref = JSON.parse(reference);
     // TODO Integrate with crossref.org api
     // if (false && typeof ref.doi === 'undefined' || ref.doi === '') {
     return (<span>
-      {ref.title !== '' ? <strong>{`"${ref.title}"`}. </strong> : null }
-      {typeof ref.authors !== 'undefined' ? <span>{ref.authors.join('; ').replace('\\o', 'ø')}. </span> : null }
-      {ref.journal !== '' ? <i>{ref.journal}, </i> : null }
-      {ref.volume !== '' ? <span>{ref.volume} </span> : null}
-      {ref.year !== '' ? <span>({ref.year}): </span> : null}
-      {ref.pages !== '' ? <span>{ref.pages}. </span> : null}
+      {(ref.title !== '' && ref.title !== null && typeof ref.title !== 'undefined') ? <strong>{`"${restoreSC(ref.title)}"`}. </strong> : null }
+      {(typeof ref.authors !== 'undefined' && ref.authors !== '' && ref.authors !== null) ? <span>{restoreSC(ref.authors.join('; '))}. </span> : null }
+      {(ref.journal !== '' && typeof ref.journal !== 'undefined' && ref.journal !== null) ? <i>{ref.journal}, </i> : null }
+      {(ref.volume !== '' && typeof ref.volume !== 'undefined' && ref.volume !== null) ? <span>{ref.volume} </span> : null}
+      {(ref.year !== '' && typeof ref.year !== 'undefined' && ref.year !== null) ? <span>({ref.year}): </span> : null}
+      {(ref.pages !== '' && typeof ref.pages !== 'undefined' && ref.pages !== null) ? <span>{ref.pages}. </span> : null}
     </span>);
   }
   return null;
