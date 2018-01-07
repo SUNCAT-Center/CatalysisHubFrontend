0a3ea87c5d6577605a1532427c7dbb4cc32a738e
Max Hoffmann <mjhoffmann@gmail.com>
2018-01-06 18:13:01 -0800

    Added forgotten import

---
+++ app/components/GeometryCanvasCifdata/index.js
@@ -0,0 +1,131 @@
+/**
+ *
+ * GeometryCanvasCifdata
+ *
+ */
+
+import React, { PropTypes } from 'react';
+import { withStyles } from 'material-ui/styles';
+import { isMobile } from 'react-device-detect';
+
+import jQuery from 'jquery';
+window.jQuery = jQuery;
+import { ChemDoodle } from 'utils/ChemDoodleWeb';
+
+const styles = () => ({
+});
+
+const initialState = {
+};
+
+class GeometryCanvasCifdata extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = {
+      orientation: 'test orientation',
+    };
+  }
+  componentDidMount() {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.innerHTML = `
+function _load_lib(url, callback){
+  id =  'load_' + url;
+  var s = document.createElement('script');
+  s.src = url;
+  s.id = id;
+  s.async = true;
+  s.onreadystatechange = s.onload = callback;
+  s.onerror = function(){console.warn("failed to load library " + url);};
+  document.getElementsByTagName("head")[0].appendChild(s);
+
+}
+// Load Libraries
+  var rotationMatrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
+
+  if(typeof cif_${this.props.selectedSystem.uniqueId} === 'undefined' ) {
+   let cif_${this.props.selectedSystem.uniqueId} 
+  }
+  if(typeof tfcanvas_${this.props.selectedSystem.uniqueId} === 'undefined' ) {
+   let tfcanvas_${this.props.selectedSystem.uniqueId} 
+  }
+  //Code
+  tfcanvas_${this.props.selectedSystem.uniqueId} = new ChemDoodle.TransformCanvas3D('${this.props.id}_view');
+  cif_${this.props.selectedSystem.uniqueId} = ChemDoodle.readCIF(\`${this.props.selectedSystem.Cifdata}\`,  ${this.props.x}, ${this.props.y}, ${this.props.z});
+  let alpha_${this.props.selectedSystem.uniqueId} = 0.; 
+  let beta_${this.props.selectedSystem.uniqueId} = 0.; 
+  let gamma_${this.props.selectedSystem.uniqueId} = 0.;
+
+  tfcanvas_${this.props.selectedSystem.uniqueId}.specs.set3DRepresentation('Ball and Stick');
+  tfcanvas_${this.props.selectedSystem.uniqueId}.specs.backgroundColor = '${this.props.color}';
+  tfcanvas_${this.props.selectedSystem.uniqueId}.specs.projectionPerspective_3D = true;
+  tfcanvas_${this.props.selectedSystem.uniqueId}.specs.atoms_displayLabels_3D = true;
+  tfcanvas_${this.props.selectedSystem.uniqueId}.specs.crystals_unitCellLineWidth = 5;
+  tfcanvas_${this.props.selectedSystem.uniqueId}.specs.shapes_color = 'black';
+  tfcanvas_${this.props.selectedSystem.uniqueId}.specs.shapes_lineWidth = 1;
+  tfcanvas_${this.props.selectedSystem.uniqueId}.loadContent([cif_${this.props.selectedSystem.uniqueId}.molecule], [cif_${this.props.selectedSystem.uniqueId}.unitCell]);
+  console.log(tfcanvas_${this.props.selectedSystem.uniqueId})
+  /*tf.rotationMatrix = ChemDoodle.lib.mat4.rotate(tf.rotationMatrix,.1, [0,1,0]); tf.updateScene()*/
+  window.addEventListener('deviceorientation', (e) => {
+  tfcanvas_${this.props.selectedSystem.uniqueId}.rotationMatrix = ChemDoodle.lib.mat4.rotate(tfcanvas_${this.props.selectedSystem.uniqueId}.rotationMatrix,(e.alpha -  alpha_${this.props.selectedSystem.uniqueId}) * 0.15, tfcanvas_${this.props.selectedSystem.uniqueId}.rototationMatrix.slice(0, 3));
+  tfcanvas_${this.props.selectedSystem.uniqueId}.rotationMatrix = ChemDoodle.lib.mat4.rotate(tfcanvas_${this.props.selectedSystem.uniqueId}.rotationMatrix,(e.beta -  beta_${this.props.selectedSystem.uniqueId}) * 0.15,  tfcanvas_${this.props.selectedSystem.uniqueId}.rototationMatrix.slice(8, 11));
+  tfcanvas_${this.props.selectedSystem.uniqueId}.rotationMatrix = ChemDoodle.lib.mat4.rotate(tfcanvas_${this.props.selectedSystem.uniqueId}.rotationMatrix,(e.gamma -  gamma_${this.props.selectedSystem.uniqueId}) * 0.15,  tfcanvas_${this.props.selectedSystem.uniqueId}.rototationMatrix.slice(4, 7));
+  alpha_${this.props.selectedSystem.uniqueId} = e.alpha;
+  beta_${this.props.selectedSystem.uniqueId} = e.beta;
+  gamma_${this.props.selectedSystem.uniqueId} = e.gamma;
+  /*console.log(e)*/
+   tfcavanas_${this.props.selectedSystem.uniqueId}.updateScene()
+
+  console.log(JSON.stringify(e));}, true);
+    `;
+    const item = document.getElementById(`${this.props.id}_view`);
+
+    item.appendChild(script);
+  }
+  render() {
+    return (
+      <div>
+        {isMobile === false ? null : <p> Experimental: touch geometry and rotate mobile device.</p> }
+        <p id={`${this.props.id}_script`} >
+          <canvas
+            id={`${this.props.id}_view`}
+            height={this.props.height}
+            width={this.props.width}
+            style={{
+              borderWidth: 0,
+              borderColor: '#000000',
+              borderStyle: 'solid',
+            }}
+          />
+        </p>
+      </div>
+    );
+  }
+}
+GeometryCanvasCifdata.defaultProps = {
+  height: Math.max(Math.min(window.innerWidth * 0.5, 600), 300),
+  width: Math.max(Math.min(window.innerWidth * 0.5, 600), 300),
+  color: '#fff',
+  cifdata: '',
+  cifurl: '',
+  x: 2,
+  y: 2,
+  z: 1,
+};
+
+GeometryCanvasCifdata.propTypes = {
+  selectedSystem: PropTypes.object,
+  system: PropTypes.object.isRequired,
+  id: PropTypes.string.isRequired,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  color: PropTypes.string,
+  classes: PropTypes.object,
+  x: PropTypes.number,
+  y: PropTypes.number,
+  z: PropTypes.number,
+};
+
+
+export default withStyles(styles, { withTheme: true })(GeometryCanvasCifdata);
