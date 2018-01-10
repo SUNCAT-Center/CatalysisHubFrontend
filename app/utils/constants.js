5a751a406c70240786563426c819681e2f41eee4
Max Hoffmann <mjhoffmann@gmail.com>
2018-01-10 09:48:59 -0800

    Linked app version to package version

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,3 +1,5 @@
+import packageJson from './package.json';
+
 module.exports = {
   backendRoot: 'https://obscure-woodland-71302.herokuapp.com/',
   /* graphQLRoot: (process.env.NODE_ENV !== 'production') ? '//localhost:5000/graphql' : '//catappdatabase.herokuapp.com/graphql', */
@@ -5,7 +7,7 @@ module.exports = {
   whiteLabel: false,
   suBranding: false,
   appBar: true,
-  version: '0.1.4',
+  version: packageJson.version,
   gaTrackingId: 'UA-109061730-1',
   apps: [
     {
