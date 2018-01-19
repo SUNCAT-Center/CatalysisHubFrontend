c9c90babbb4b994174c4e6dc4dfaae768dbe92b5
Max Hoffmann <mjhoffmann@gmail.com>
2018-01-19 10:33:48 -0800

    Little screen size tweaks

--- app/containers/ActivityMapsPage/ActivityMapPlot.js
+++ app/containers/ActivityMapsPage/ActivityMapPlot.js
@@ -66,14 +66,14 @@ class ActivityMapOer extends React.Component { // eslint-disable-line react/pref
     return (
       <div>
         <div ref={(el) => { this.instance = el; }}>
-          <h2 style={{ marginTop: '100px' }}>Activity Map</h2>
+          <h2>Activity Map</h2>
           <p>Data generously provided by M. Bajdich, unpublished, 2017.</p>
           <Plot
             {...plotlydata}
             layout={{
               hovermode: 'closest',
-              height: Math.max(Math.min(window.innerHeight * 0.8, Number.POSITIVE_INFINITY), 300),
-              width: Math.max(Math.min(window.innerWidth * 0.8, 1150), 300),
+              height: Math.max(Math.min(window.innerHeight * 0.6, Number.POSITIVE_INFINITY), 120),
+              width: Math.max(Math.min(window.innerWidth * 0.8, 1150), 320),
               margin: { l: 20, r: 20, b: 10, t: 10 },
             }}
             onClick={(event) => { this.props.clickDot(event); }}
