033f845a224a0087c2a896e64fb3a5cedb0f264f
Max Hoffmann <mjhoffmann@gmail.com>
2018-01-29 13:45:22 -0800

    Added temporary url for local flask app

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -2,6 +2,7 @@ import packageJson from './package.json';
 
 module.exports = {
   backendRoot: 'https://obscure-woodland-71302.herokuapp.com/',
+  flaskRoot: 'http://localhost:5000',
   /* graphQLRoot: (process.env.NODE_ENV !== 'production') ? '//localhost:5000/graphql' : '//catappdatabase.herokuapp.com/graphql', */
   graphQLRoot: '//catappdatabase.herokuapp.com/graphql',
   whiteLabel: false,
