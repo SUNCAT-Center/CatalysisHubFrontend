a3039e5592c74925b3bcc2566cc271592f7a7fe4
Max Hoffmann <mjhoffmann@gmail.com>
2018-02-01 18:08:39 -0800

    Added setting page for site-wide settings

---
+++ app/containers/Settings/tests/selectors.test.js
@@ -0,0 +1,10 @@
+// import { fromJS } from 'immutable';
+// import { makeSelectSettingsDomain } from '../selectors';
+
+// const selector = makeSelectSettingsDomain();
+
+describe('makeSelectSettingsDomain', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
