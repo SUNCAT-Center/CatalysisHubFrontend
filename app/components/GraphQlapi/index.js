98c3f75cf24cf5a6f3822fc74b5afc2cb78cfc04
Max Hoffmann <mjhoffmann@gmail.com>
2018-02-05 17:52:20 -0800

    Persist display of graphql intro with cookies

--- app/components/GraphQlapi/index.js
+++ app/components/GraphQlapi/index.js
@@ -5,37 +5,66 @@
  */
 
 import React from 'react';
-// import styled from 'styled-components';
+import PropTypes, { instanceOf } from 'prop-types';
 
 import IFrame from 'react-iframe';
 import Card, { CardActions, CardContent } from 'material-ui/Card';
 import Button from 'material-ui/Button';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
 
+import { withStyles } from 'material-ui/styles';
+import { withCookies, Cookies } from 'react-cookie';
 
 import { graphQLRoot } from 'utils/constants';
+
+const styles = (theme) => ({
+  button: {
+    margin: theme.spacing.unit * 2.5,
+  },
+});
 
 class GraphQlapi extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
     super(props);
     this.state = {
-      showInfo: true,
+      graphqlShowInfo: ((this.props.cookies.get('graphqlShowInfo') || 'true') === 'true'),
     };
     this.handleGotit = this.handleGotit.bind(this);
   }
 
-  handleGotit() {
+  handleGotit(value) {
     this.setState({
-      showInfo: false,
+      graphqlShowInfo: value,
     });
+    this.props.cookies.set('graphqlShowInfo', value);
   }
   render() {
     return (
       <div>
-        <h2>CatApp Database API</h2>
-        {this.state.showInfo === false ? null :
+        <Grid container direction="row" justify="space-between">
+          <Grid item>
+            <h2>CatApp Database API</h2>
+          </Grid>
+          <Grid item>
+            {this.state.graphqlShowInfo === true ? null :
+            <Button className={this.props.classes.button} color="primary" onClick={() => { this.handleGotit(true); }}>Help!</Button>
+        }
+          </Grid>
+        </Grid>
+        {this.state.graphqlShowInfo === false ? null :
         <Card style={{ marginBottom: 20 }}>
           <CardContent>
-            <h3>Welcome to the GraphQL API</h3>
+            <Grid container direction="row" justify="space-between">
+              <Grid item >
+                <h3>Welcome to the GraphQL API</h3>
+              </Grid>
+              <Grid item >
+                <CardActions>
+                  <Button color="primary" onClick={() => { this.handleGotit(false); }}>Got it! </Button>
+                </CardActions>
+              </Grid>
+            </Grid>
             <div>You can write you own queries and submit them with<br /> <pre>Command + Enter</pre></div>
             <div>Some examples are</div>
             <ul>
@@ -45,7 +74,7 @@ class GraphQlapi extends React.Component { // eslint-disable-line react/prefer-s
             </ul>
             <div>{"Open the 'Docs' tab on the right to find a full documentation."}</div>
             <div>Alternatively you query from the command line using <pre style={{ display: 'inline' }}>curl</pre>:
-              <pre>{ `curl -XPOST http://catappdatabase.herokuapp.com/graphql --data 'query={systems(last: 10 ) {
+                  <pre>{ `curl -XPOST http://catappdatabase.herokuapp.com/graphql --data 'query={systems(last: 10 ) {
   edges {
     node {
       energy Cifdata
@@ -55,31 +84,43 @@ class GraphQlapi extends React.Component { // eslint-disable-line react/prefer-s
             </div>
             <div>Or you can send the same query from a python script like so</div>
             <pre>{'import requests\n' +
-              `requests.post(root, {'query':
+                `requests.post(root, {'query':
               '{systems(last: 5) { edges { node { energy PublicationTitle InputFile(format: "espresso-in") } } }}
               }).json()`}</pre>
+            <Grid container direction="row" justify="flex-end">
+              <Grid item >
+                <CardActions>
+                  <Button color="primary" onClick={() => { this.handleGotit(false); }}>Got it! </Button>
+                </CardActions>
+              </Grid>
+            </Grid>
           </CardContent>
-          <CardActions>
-            <Button dense color="primary" onClick={this.handleGotit}>Got it! </Button>
-          </CardActions>
         </Card>
         }
 
-        <IFrame
-          url={graphQLRoot}
-          width="100%"
-          height="80vh"
-          position="relative"
-          display="initial"
-          allowFullScreen
-        />
+        <Paper>
+          <IFrame
+            url={graphQLRoot}
+            width="100%"
+            height="80vh"
+            position="relative"
+            display="initial"
+            allowFullScreen
+          />
+        </Paper>
       </div>
     );
   }
 }
 
 GraphQlapi.propTypes = {
+  cookies: instanceOf(Cookies),
+  classes: PropTypes.object,
 
 };
 
-export default GraphQlapi;
+export default withCookies(
+  withStyles(styles, { withTheme: true })(
+    GraphQlapi
+  )
+);
