ac1c2b95e802c7acb063a2161c13cbfc2ef7c006
Max Hoffmann <mjhoffmann@gmail.com>
2018-02-07 09:52:10 -0800

    Fixed wording

--- app/components/GeometryCanvasCifdata/index.js
+++ app/components/GeometryCanvasCifdata/index.js
@@ -150,7 +150,7 @@ function _load_lib(url, callback){
   render() {
     return (
       <div>
-        {isMobile === false ? null : <div> <Md3dRotation />{'\u00A0\u00A0'} Tilt device to rotate.</div> }
+        {isMobile === false ? null : <div> <Md3dRotation />{'\u00A0\u00A0'} Tilt phone to rotate.</div> }
         <Paper height={14}>
           <p id={`${this.props.id}_script`} >
             <canvas
