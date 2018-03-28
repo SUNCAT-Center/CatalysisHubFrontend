5e32fc1adc8b571e34342e8a72d384156c38d609
Max Hoffmann <mjhoffmann@gmail.com>
2018-03-27 17:25:21 -0700

    Updated data for CO2RR

--- app/containers/ActivityMapsPage/plot_data/index.js
+++ app/containers/ActivityMapsPage/plot_data/index.js
@@ -1,6 +1,7 @@
 import NRR from './NRR.json';
 import OER from './OER.json';
 import ORR from './ORR.json';
+import CO2RR from './CO2RR.json';
 // eslint-disable-next-line camelcase
 import CO_Hydrogenation from './CO_Hydrogenation.json';
 
@@ -8,6 +9,7 @@ const plotlyData = {
   NRR,
   OER,
   ORR,
+  CO2RR,
   // eslint-disable-next-line camelcase
   CO_Hydrogenation,
 };
