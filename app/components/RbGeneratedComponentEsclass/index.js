e1bb141e6a3e65f696088b4895da144430e53770
Max Hoffmann <mjhoffmann@gmail.com>
2018-04-05 13:45:39 -0700

    Removed a blank line

---
+++ app/components/RbGeneratedComponentEsclass/index.js
@@ -0,0 +1,28 @@
+/**
+*
+* RbGeneratedComponentEsclass
+*
+*/
+
+import React from 'react';
+
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
+
+
+class RbGeneratedComponentEsclass extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <FormattedMessage {...messages.header} />
+      </div>
+    );
+  }
+}
+
+RbGeneratedComponentEsclass.propTypes = {
+
+};
+
+
+export default RbGeneratedComponentEsclass;
