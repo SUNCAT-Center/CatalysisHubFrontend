d905e12ce49c1ebbae994d9b4bd232337768f71a
Max Hoffmann <mjhoffmann@gmail.com>
2018-04-05 23:59:35 -0700

    Fixed more tests

--- app/components/ElementBox/tests/index.test.js
+++ app/components/ElementBox/tests/index.test.js
@@ -4,10 +4,10 @@ import { shallow } from 'enzyme';
 import ElementBox from '../index';
 
 describe('<ElementBox />', () => {
-  it('it should render a styled div', () => {
+  it('it should render a styled ElementBox', () => {
     const renderedComponent = shallow(
       <ElementBox label="test" clickElement={() => {}} />
     );
-    expect(renderedComponent.text()).toContain('div');
+    expect(renderedComponent.text()).toContain('ElementBox');
   });
 });
