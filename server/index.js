34b5b80ad1206dbe13dcce2d87a35703fedcb637
Max Hoffmann <mjhoffmann@gmail.com>
2018-04-30 16:03:05 -0700

    Added font-src settings

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ app.use(helmet());
 app.use(csp({
   directives: {
     connectSrc: ["'self'", 'api.catalysis-hub.org'],
+    fontSrc: ["'self'", 'fonts.googleapis.com'],
     scriptSrc: ["'self'", 'api.catalysis-hub.org', 'www.google-analytics.com'],
     stylesSrc: ["'self'", 'api.catalysis-hub.org'],
     defaultSrc: ["'self'", 'api.catalysis-hub.org'],
