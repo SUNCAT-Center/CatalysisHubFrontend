629f2c64b6bf382d096d75249f85dc00da8e6143
Max Hoffmann <mjhoffmann@gmail.com>
2018-04-30 15:08:14 -0700

    Reset server/index

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 /* eslint consistent-return:0 */
 
 const express = require('express');
-/* const helmet = require('helmet');*/
-/* const sslRedirect = require('heroku-ssl-redirect');*/
 const logger = require('./logger');
 
 const argv = require('minimist')(process.argv.slice(2));
@@ -14,9 +12,6 @@ const app = express();
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
-
-/* app.use(helmet());*/
-/* app.use(sslRedirect());*/
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
