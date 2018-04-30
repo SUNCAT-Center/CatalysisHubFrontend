ead4fe7198af2364563ca75f12fe021a65c022d8
Max Hoffmann <mjhoffmann@gmail.com>
2018-04-30 14:58:13 -0700

    Undo helmet and ssl redirect

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 /* eslint consistent-return:0 */
 
 const express = require('express');
-const helmet = require('helmet');
-const sslRedirect = require('heroku-ssl-redirect');
+/* const helmet = require('helmet');*/
+/* const sslRedirect = require('heroku-ssl-redirect');*/
 const logger = require('./logger');
 
 const argv = require('minimist')(process.argv.slice(2));
@@ -15,8 +15,8 @@ const app = express();
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
 
-app.use(helmet());
-app.use(sslRedirect());
+/* app.use(helmet());*/
+/* app.use(sslRedirect());*/
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
