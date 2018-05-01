45e128743897f1bc7a50cf12ace24123b26fce12
Max Hoffmann <mjhoffmann@gmail.com>
2018-04-30 17:42:53 -0700

    Changed  apiRoot to https

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.use(csp({
       'api.catalysis-hub.org',
       'catapp-staging.herokuapp.com',
       'ichemlabs.cloud.chemdoodle.com',
+      'localhost:5000',
     ],
     fontSrc: [
       "'self'",
