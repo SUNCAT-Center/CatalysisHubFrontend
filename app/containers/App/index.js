c89e43508dbce6a43713c48466256d3dfe9d4c42
Max Hoffmann <mjhoffmann@gmail.com>
2018-05-03 00:34:34 -0700

    Made page title dynamic

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -480,8 +480,9 @@ class App extends React.Component {
             >
               <Helmet
                 className={this.props.classes.helmet}
-                titleTemplate="%s - Catalysis-Hub.org"
-                defaultTitle="Catalysis-Hub.org"
+                titleTemplate="%s - Catalysis-Hub.Org"
+                defaultTitle="Catalysis-Hub.Org"
+                title={`${(_.startCase(this.props.location.pathname.replace('/', '')).match(/[A-Z][a-z]+/g) || ['Index']).join(' ')} - Catalysis-Hub.Org`}
                 meta={[
                   { name: 'description', content: `Catalysis-Hub.org is a frontend for browsing the SUNCAT CatApp database containing thousands of first-principles calculations related to heterogeneous catalysis reactions on surface systems. Its goal is to allow comprehensive and user-friendly access to raw quantum chemical simulations guided by heterogeneous catalysis concepts and commonly used graphical representations such as scaling relations and activity maps. All reaction energies are derived from periodic plane-wave density functional theory calculations. An increasing number of calculations contain the corresponding optimized geometry as well as further calculational details such as exchange-correlation (XC) functional, basis set quality, and k-point sampling. Ultimately, the goal is to provide fully self-contained data for predicting experimental observations from electronic structure calculations and using software packages such as Quantum Espresso, GPAW, VASP, and FHI-aims. Input and output with other codes is supported through the Atomic Simulation Environment (ASE). It may also serve as a natural starting point for training and developing machine-learning based approaches accelerating quantum chemical simulations.
       Features include search for specific reaction energies, transition states, structures, exploration of scaling relations, activity maps, Pourbaix diagrams and machine learning models, as well as generation of novel bulk and surface structures. Calculations are linked to peer-review publications where available. The database can be queried via a GraphQL API that can also be accessed directly.
