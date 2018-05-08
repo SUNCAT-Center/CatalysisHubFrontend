abc1ae88ce4dce97ae536046b1cdc12352d0a891
Max Hoffmann <mjhoffmann@gmail.com>
2018-05-08 08:01:23 -0700

    Added URL resolver for COD repository

--- app/containers/PrototypeSearch/index.js
+++ app/containers/PrototypeSearch/index.js
@@ -79,6 +79,8 @@ const getGeometryUrl = (repository, handle) => {
     return `http://rruff.geo.arizona.edu/AMS/CIF_text_files/${handle}`;
   } else if (repository === 'catalysis-hub') {
     return `http://api.catalysis-hub.org/graphql?query=%7Bsystems(uniqueId%3A%22${handle}%22)%20%7B%0A%20%20edges%20%7B%0A%20%20%20%20node%20%7B%0A%20%20%20%20%20%20id%0A%20%20%20%20%20%20uniqueId%0A%20%20%20%20%20%20energy%0A%20%20%20%20%20%20keyValuePairs%0A%20%20%20%20%20%20InputFile(format%3A%22cif%22)%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%7D`;
+  } else if (repository === 'COD') {
+    return `http://www.crystallography.net/cod/${handle}.html`;
   }
   return 'http://www.catalysis-hub.org/Error: Unknown repository';
 };
