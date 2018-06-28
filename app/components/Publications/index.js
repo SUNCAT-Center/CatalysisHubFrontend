e215c351d8de268d66bdf69b8b556ff4df0f4030
Max Hoffmann <mjhoffmann@gmail.com>
2018-06-28 12:05:38 -0700

    Refactored pretty-print-reference function out of publications list

--- app/components/Publications/index.js
+++ app/components/Publications/index.js
@@ -25,6 +25,7 @@ import { withStyles } from 'material-ui/styles';
 
 import axios from 'axios';
 import { newGraphQLRoot } from 'utils/constants';
+import { prettyPrintReference } from 'utils/functions';
 
 import PublicationView from 'components/PublicationView';
 import PublicationSystems from './publicationSystems';
@@ -60,44 +61,6 @@ const styles = (theme) => ({
     marginTop: theme.spacing.unit,
   },
 });
-
-const restoreSC = (str) => {
-  let res = str;
-  if (str === null || typeof str === 'undefined') {
-    return '';
-  }
-  if (typeof str === 'object') {
-    res = str.join(' ');
-  }
-  return res
-    .replace('{\\o}', 'ø')
-    .replace('\\o', 'ø')
-    .replace('{"A}', 'Ä')
-    .replace('{"U}', 'Ü')
-    .replace('{"O}', 'Ö')
-    .replace('{"a}', 'ä')
-    .replace('{"u}', 'ü')
-    .replace('{"o}', 'ö')
-    .replace('{\\ss}', 'ß')
-    .replace('--', '–')
-    .replace('Norskov', 'Nørskov')
-
-    .replace('{', '')
-    .replace('}', '');
-};
-
-const prettyPrintReference = (ref) =>
-  // TODO Integrate with crossref.org api
-  // if (false && typeof ref.doi === 'undefined' || ref.doi === '') {
-   (<span>
-     {(ref.title !== '' && ref.title !== null && typeof ref.title !== 'undefined') ? <strong>{`"${restoreSC(ref.title)}"`}. </strong> : null }
-     {(typeof ref.authors !== 'undefined' && ref.authors !== '' && ref.authors !== null) ? <span>{restoreSC(typeof ref.authors === 'string' ? JSON.parse(ref.authors).join('; ') : ref.authors.join('; '))}. </span> : null }
-     {(ref.journal !== '' && typeof ref.journal !== 'undefined' && ref.journal !== null) ? <i>{ref.journal}, </i> : null }
-     {(ref.volume !== '' && typeof ref.volume !== 'undefined' && ref.volume !== null) ? <span>{ref.volume} </span> : null}
-     {(ref.year !== '' && typeof ref.year !== 'undefined' && ref.year !== null) ? <span>({ref.year}): </span> : null}
-     {(ref.pages !== '' && typeof ref.pages !== 'undefined' && ref.pages !== null) ? <span>{ref.pages}. </span> : null}
-   </span>);
-
 
 class Publications extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
