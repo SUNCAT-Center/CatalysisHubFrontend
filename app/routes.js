7a99ebde8979a0bbd7e5aaf1fc1082649883eb4b
Max Hoffmann <mjhoffmann@gmail.com>
2018-06-29 15:48:23 -0700

    Added privacy policy

--- app/routes.js
+++ app/routes.js
@@ -348,6 +348,14 @@ export default function createRoutes(store) {
           .catch(errorLoading);
       },
     }, {
+      path: '/privacyPolicy',
+      name: 'privacyPolicy',
+      getComponent(location, cb) {
+        import('components/PrivacyPolicy')
+          .then(loadModule(cb))
+          .catch(errorLoading);
+      },
+    }, {
       path: '*',
       name: 'notfound',
       getComponent(nextState, cb) {
