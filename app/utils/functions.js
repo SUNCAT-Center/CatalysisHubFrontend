1971ce3507981c6bab8b245c35b12d87b258a6b8
Max Hoffmann <mjhoffmann@gmail.com>
2018-07-04 08:56:38 -0700

    Polished pretty-printing of references

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -30,15 +30,27 @@ export const restoreSC = (str) => {
     .replace('}', '');
 };
 
+export function toTitleCase(str) {
+  return str.replace(
+        /\w\S*/g,
+        (txt) => {
+          if (txt.match(/^[A-Z0-9]+$/g) !== null) {
+            return txt;
+          }
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
 // Print a publication reference from JSON object.
 export const prettyPrintReference = (ref) =>
   // TODO Integrate with crossref.org api
   // if (false && typeof ref.doi === 'undefined' || ref.doi === '') {
    (<span>
-     {(ref.title !== '' && ref.title !== null && typeof ref.title !== 'undefined') ? <strong>{`"${restoreSC(ref.title)}"`}. </strong> : null }
-     {(typeof ref.authors !== 'undefined' && ref.authors !== '' && ref.authors !== null) ? <span>{restoreSC(typeof ref.authors === 'string' ? JSON.parse(ref.authors).join('; ') : ref.authors.join('; '))}. </span> : null }
+     {(ref.title !== '' && ref.title !== null && typeof ref.title !== 'undefined') ? <strong>{`"${toTitleCase(restoreSC(ref.title))}"`}. </strong> : null }
+     {(typeof ref.authors !== 'undefined' && ref.authors !== '' && ref.authors !== null) ? <span> {restoreSC(typeof ref.authors === 'string' ? JSON.parse(ref.authors).join('; ').replace(/[,;](?! )/g, ', ') : ref.authors.join('; ').replace(/[,;](?! )/g, ', '))}. </span> : null }
      {(ref.journal !== '' && typeof ref.journal !== 'undefined' && ref.journal !== null) ? <i>{ref.journal}, </i> : null }
      {(ref.volume !== '' && typeof ref.volume !== 'undefined' && ref.volume !== null) ? <span>{ref.volume} </span> : null}
-     {(ref.year !== '' && typeof ref.year !== 'undefined' && ref.year !== null) ? <span>({ref.year}): </span> : null}
+     {(ref.year !== '' && typeof ref.year !== 'undefined' && ref.year !== null) ? <span>({ref.year}). </span> : null}
      {(ref.pages !== '' && typeof ref.pages !== 'undefined' && ref.pages !== null) ? <span>{ref.pages}. </span> : null}
    </span>);
