fb782cea8ce006e84f7156c8d89f1abd6d46dcf6
Max Hoffmann <mjhoffmann@gmail.com>
2018-07-07 10:07:08 -0700

    Refactored css into own file

--- app/components/Publications/index.js
+++ app/components/Publications/index.js
@@ -34,45 +34,7 @@ import PublicationView from 'components/PublicationView';
 import PublicationSystems from './publicationSystems';
 // import PublicationReactions from './publicationReactions';
 
-const styles = (theme) => ({
-  publicationAction: {
-    margin: theme.spacing.unit,
-    height: 6,
-    backgroundColor: _.get(theme, 'palette.sandhill.50'),
-    '&:hover': {
-      backgroundColor: _.get(theme, 'palette.sandhill.300'),
-    },
-  },
-  outboundLink: {
-    textDecoration: 'none',
-  },
-  publicationEntry: {
-    textAlign: 'left',
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '14px',
-    },
-  },
-  publicationYear: {
-    textAlign: 'left',
-    [theme.breakpoints.down('sm')]: {
-      textAlign: 'center',
-    },
-  },
-  paper: {
-    padding: theme.spacing.unit,
-    marginBottom: theme.spacing.unit,
-    marginTop: theme.spacing.unit,
-  },
-  smallPaper: {
-    padding: theme.spacing.unit,
-    marginTop: theme.spacing.unit,
-    paddingBottom: 0,
-  },
-  publicationActions: {
-    marginTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-  },
-});
+import { styles } from './styles';
 
 class Publications extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
---
+++ app/components/Publications/styles.js
@@ -0,0 +1,40 @@
+import { _ } from 'lodash';
+export const styles = (theme) => ({
+  publicationAction: {
+    margin: theme.spacing.unit,
+    height: 6,
+    backgroundColor: _.get(theme, 'palette.sandhill.50'),
+    '&:hover': {
+      backgroundColor: _.get(theme, 'palette.sandhill.300'),
+    },
+  },
+  outboundLink: {
+    textDecoration: 'none',
+  },
+  publicationEntry: {
+    textAlign: 'left',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '14px',
+    },
+  },
+  publicationYear: {
+    textAlign: 'left',
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'center',
+    },
+  },
+  paper: {
+    padding: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
+    marginTop: theme.spacing.unit,
+  },
+  smallPaper: {
+    padding: theme.spacing.unit,
+    marginTop: theme.spacing.unit,
+    paddingBottom: 0,
+  },
+  publicationActions: {
+    marginTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+  },
+});
