be8c5ab2d254d87b2d37c47add1466cd52dc2e2c
Max Hoffmann <mjhoffmann@gmail.com>
2018-07-07 10:13:09 -0700

    Refactored css into own file

--- app/components/GeometryCanvasWithOptions/index.js
+++ app/components/GeometryCanvasWithOptions/index.js
@@ -30,62 +30,10 @@ import { apiRoot } from 'utils/constants';
 import GeometryCanvasCifdata from 'components/GeometryCanvasCifdata';
 
 import * as actions from './actions';
+import { styles } from './styles';
 
 export const outputFormats = ['abinit', 'castep-cell', 'cfg', 'cif', 'dlp4', 'eon', 'espresso-in', 'extxyz', 'findsym',
   'gen', 'gromos', 'json', 'jsv', 'nwchem', 'proteindatabank', 'py', 'turbomole', 'v-sim', 'vasp', 'xsf', 'xyz'];
-
-
-const styles = (theme) => ({
-  avatar: {
-    fontColor: '#000000',
-    fontSize: 18,
-    backgroundColor: '#ffffff',
-    marginLeft: -14,
-    marginRight: -14,
-    marginTop: -5,
-    textTranformation: 'none',
-    [theme.breakpoints.down('sm')]: {
-      fontSize: 12,
-    },
-  },
-  button: {
-    margin: theme.spacing.unit,
-  },
-  fsIconButton: {
-    fontColor: '#000000',
-    backgroundColor: '#ffffff',
-    marginTop: theme.spacing.unit,
-    [theme.breakpoints.down('sm')]: {
-      fontSize: 12,
-      height: 15,
-      width: 15,
-    },
-    [theme.breakpoints.down('xs')]: {
-      fontSize: 12,
-      height: 10,
-      width: 10,
-    },
-  },
-  iconButton: {
-    fontColor: '#000000',
-    backgroundColor: '#ffffff',
-    [theme.breakpoints.down('sm')]: {
-      fontSize: 12,
-      height: 15,
-      width: 15,
-    },
-    [theme.breakpoints.down('xs')]: {
-      fontSize: 12,
-      height: 10,
-      width: 10,
-    },
-  },
-  iconButtonIcon: {
-    fontColor: '#000000',
-    backgroundColor: '#ffffff',
-    marginTop: 7,
-  },
-});
 
 const initialState = {
   open: false,
