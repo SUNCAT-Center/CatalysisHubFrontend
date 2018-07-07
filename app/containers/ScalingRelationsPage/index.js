70f59a969f400693381b1fb6072323bd8eb7807e
Max Hoffmann <mjhoffmann@gmail.com>
2018-07-07 10:46:30 -0700

    Refactored styles into extra file

--- app/containers/ScalingRelationsPage/index.js
+++ app/containers/ScalingRelationsPage/index.js
@@ -24,6 +24,7 @@ import { newGraphQLRoot } from 'utils/constants';
 
 import ReactionAutosuggest from './ReactionAutosuggest';
 import StructureView from './StructureView';
+import { styles } from './styles';
 
 const initialState = {
   plotTitle: '',
@@ -55,50 +56,6 @@ const initialState = {
   geometries: [],
 };
 
-const styles = (theme) => ({
-  mainPaper: {
-    minHeight: 200,
-    padding: 15,
-  },
-  container: {
-    flexGrow: 1,
-    position: 'relative',
-    float: 'left',
-    display: 'inline-block',
-    whiteSpace: 'nowrap',
-  },
-  input: {
-    padding: '10 20 10 20',
-    float: 'left',
-    display: 'inline-block',
-    whiteSpace: 'nowrap',
-  },
-  suggestionsContainerOpen: {
-    position: 'absolute',
-    marginTop: theme.spacing.unit,
-    marginBottom: theme.spacing.unit * 3,
-    left: 0,
-    right: 0,
-    zIndex: 10,
-  },
-  suggestion: {
-    display: 'block',
-    zIndex: 10,
-  },
-  suggestionsList: {
-    margin: 0,
-    padding: 0,
-    listStyleType: 'none',
-    zIndex: 10,
-  },
-  textField: {
-    width: '100%',
-  },
-  progressBar: {
-    marginTop: theme.spacing.unit,
-    marginBottom: theme.spacing.unit,
-  },
-});
 
 function reactionQuery(reactants, products) {
   return {
