524453caee8c31afe854832b5dc171df535a5ba4
Max Hoffmann <mjhoffmann@gmail.com>
2018-09-28 01:34:27 -0700

    added  a handy link

--- app/containers/PrototypeSearch/header.js
+++ app/containers/PrototypeSearch/header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router';
 
 import ReactGA from 'react-ga';
 
 import Grid from 'material-ui/Grid';
+import Button from 'material-ui/Button';
 import { withStyles } from 'material-ui/styles';
 
 import { styles } from './styles';
@@ -14,6 +16,20 @@ class Header extends React.Component { // eslint-disable-line react/prefer-state
       <Grid container direction="row" justify="space-between">
         <Grid item>
           <h1>Prototype Search</h1>
+        </Grid>
+        <Grid item>
+
+          <Link
+            to={'/catKitDemo'}
+            className={this.props.classes.outboundLink}
+          >
+            <Button
+              raised
+              className={this.props.classes.publicationAction}
+            >
+                        CatKit Slab Generator
+                    </Button>
+          </Link>
         </Grid>
         <Grid>
           <div
